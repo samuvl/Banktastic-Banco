2fb62c8cc9240ee5207bcfadc0ce6935c862907e
Lliurex <Lliurex@172.16.205.116>
2016-02-09 14:40:07 +0100

    Transacciones validadas y login innecesario  - PIN 2045

---
+++ web/scroll.js
@@ -0,0 +1,58 @@
+// inicialización de valores
+var delta = 0;
+var currentSlideIndex = 0;
+var scrollThreshold = 1;
+var numSlides = 3;
+
+
+function elementScroll(e) {
+    // --- Scrolling up ---
+    if (e.originalEvent.detail < 0 || e.originalEvent.wheelDelta > 0) {
+        delta--;
+        if (Math.abs(delta) >= scrollThreshold) {
+            prevSlide();
+        }
+    }
+    // --- Scrolling down ---
+    else {
+        delta++;
+        if (delta >= scrollThreshold) {
+            nextSlide();
+        }
+    }
+    // Prevent page from scrolling
+    return false;
+}
+
+
+function showSlide() {
+    // reset
+    delta = 0;
+
+    // Mostrar el slide actual y ocultar los hermanos
+    $(".slide").eq(currentSlideIndex).fadeIn().siblings().fadeOut();
+}
+
+
+function prevSlide() {
+    currentSlideIndex--;
+
+    if (currentSlideIndex < 0) {
+        currentSlideIndex = 0;
+    }
+    showSlide();
+}
+
+function nextSlide() {
+    currentSlideIndex++;
+
+    if (currentSlideIndex > numSlides) {
+        currentSlideIndex = numSlides;
+    }
+
+    showSlide();
+}
+
+$(window).on({
+    'DOMMouseScroll mousewheel': elementScroll
+});
