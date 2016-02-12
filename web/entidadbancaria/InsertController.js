76f6c38068936b310923d1b188d4205b9951a5f1
ralliv <villarmoto@gmail.com>
2016-02-12 18:21:12 +0100

    Villar commit: Validaciones AngularJs
    - Ahora todas las validaciones aparecen debajo del formulario con el mismo formato
    - Si alguno de los campos esta vacio o no correcto no envia la peticion a la api.

--- web/entidadbancaria/InsertController.js
+++ web/entidadbancaria/InsertController.js
@@ -5,16 +5,48 @@ function InsertController($scope, entidadBancariaService, $location, $window) {
     $scope.entidadBancaria = {};
 
     $scope.ok = function () {
-        entidadBancariaService.insert($scope.entidadBancaria).then(function (result) {
-            alert("Entidad Insertada con Éxito con el nombre: " + $scope.entidadBancaria.nombre);
-            $location.url("/find");
-        }, function (result) {
-            if (result.status === 500) {
-                alert("Ha fallado la petición. Estado HTTP:" + result.status);
-            } else {
-                $scope.businessMessages = result.data;
+
+        var formulario = $scope.formulario;
+
+        if (formulario.$invalid) {
+            $scope.businessMessages = [];
+
+            //compruebo los requeridos y los meto en la lista de mensajes de error.
+            var x;
+            camposRequeridos = formulario.$error.required;
+            for (x in camposRequeridos) {
+                $scope.businessMessages.push({"fieldName": camposRequeridos[x].$name, "mensaje": "campo requerido"});
             }
-        });
+
+            //compruebo las validaciones especificas de cada de cada campo
+            if (formulario.nombre.$error.minlength || formulario.nombre.$error.maxlength) {
+                $scope.businessMessages.push({"fieldName": formulario.nombre.$name, "mensaje": "Nombre debe tener minimo 2 y maximo 40 carácteres"});
+            }
+
+            if (formulario.codigoEntidad.$error.minlength || formulario.codigoEntidad.$error.maxnlength) {
+                $scope.businessMessages.push({"fieldName": formulario.nombre.$name, "mensaje": "Nombre debe tener minimo 2 y maximo 50 carácteres"});
+            }
+
+            if (formulario.direccion.$error.minlength || formulario.direccion.$error.maxnlength) {
+                $scope.businessMessages.push({"fieldName": formulario.nombre.$name, "mensaje": "Nombre debe tener minimo 2 y maximo 40 carácteres"});
+            }
+
+        } else {
+
+            entidadBancariaService.insert($scope.entidadBancaria).then(function (result) {
+                alert("Entidad Insertada con Éxito con el nombre: " + $scope.entidadBancaria.nombre);
+                $location.url("/find");
+            }, function (result) {
+                if (result.status === 500) {
+                    alert("Ha fallado la petición. Estado HTTP:" + result.status);
+                } else {
+                    $scope.businessMessages = result.data;
+                }
+            });
+
+        }
+
+
 
     };
 
